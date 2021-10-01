75a4f9e1e6ccc9a20c6d8732fd285fff72cd6f83
cmduquer <cmduquer@gmail.com>
2021-10-01 09:54:37 -0500

    first commit

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var handleErrors = require('./modules/middleware/handleErrors');
+var { BadRequest } = require('./modules/util/errors');
+
 
 var app = express();
+
+var MongoDBUtil = require('./modules/mongodb/mongodb.module').MongoDBUtil;
+var CustomerController = require('./modules/customer/customer.module')().CustomerController;
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+MongoDBUtil.init();
+
+app.use('/customers', CustomerController);
 
 app.get('/', function (req, res) {
   var pkg = require(path.join(__dirname, 'package.json'));
@@ -26,7 +36,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,6 +47,8 @@ app.use(function(err, req, res, next) {
     message: res.locals.message,
     error: res.locals.error
   });
-});
+});*/
+
+app.use(handleErrors);
 
 module.exports = app;
